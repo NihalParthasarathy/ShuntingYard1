cc7c57245710b61c2c4a1759dd0a47549d7c3dab
U-BSD\412657 <412657@SHS-10D9C06E3.beaverton.k12.or.us>
2022-02-15 10:47:15 -0800

    Finnished Sunting yard with bug

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void push(Node* &head, char value);
 char peek(Node* top);
+void pop(Node* &head);
 void enqueue(Queue* &front, Queue* &tail, char value);
 void printQueue(Queue* top);
 
@@ -20,15 +21,18 @@ int main() {
   cin.getline(input, 200);
   char tempArray[200];
   int count = 0;
+  
   for (int i = 0; i <strlen(input); i++) {
     if (input[i] != ' ') {
       tempArray[count] = input[i];
       count++;
     }
   }
+  
   for (int i = 0; i <strlen(input); i++) {
     input[i] = tempArray[i];
   }
+  
   for (int i = 0; i < strlen(input); i++) {
     if (isdigit(input[i])) {
 	cout << "it is a digit" << endl;
@@ -36,13 +40,31 @@ int main() {
 	enqueue(front, tail, a);
 	printQueue(front);
     }
-    else if (input[i] == '+' || input[i] == '-' || input[i] == '/' || input[i] == '*' || input[i] == '(' || input[i] == ')' || input[i] == '^') {
+    else if (input[i] == '+' || input[i] == '-' || input[i] == '/' || input[i] == '*' || input[i] == '(' || input[i] == '^') {
       cout << "Is an operation" << endl;
       char a = input[i];
       push(head, a);
       cout << peek(head) << endl;
     }
+    else if (input[i] == ')') {
+      cout << "Is a right parenthsis" << endl;
+      char data;
+      while (head->value != '(') {
+	data = peek(head);
+	pop(head);
+	cout << data << endl;
+	enqueue(front, tail, data);
+      }
+      pop(head);
+    }
   }
+  while (head != NULL) {
+    char data;
+    data = peek(head);
+    pop(head);
+    enqueue(front, tail, data);
+  }
+  printQueue(front);
 }
 
 void push(Node* &head, char value) {
@@ -52,8 +74,16 @@ void push(Node* &head, char value) {
   head = temp;
 }
 
-void pop() {
-
+void pop(Node* &head) {
+  if (head == NULL) {
+    cout << "Nothing to pop" << endl;
+  }
+  else {
+    Node* temp;
+    temp = head;
+    head = temp->next;
+    free(temp);
+  }
 }
 
 char peek(Node* top) {
